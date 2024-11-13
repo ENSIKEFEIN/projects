47322761d4bcbc98a63942812c8c48cf212a1c38
caleb <keghamngum@gmail.com>
2024-11-13 11:54:44 +0100

    i think this is my last commit

---
+++ trial.py
@@ -0,0 +1,17 @@
+
+'''def greet(name):
+    print(F"hello, {name}")
+greet("caleb")
+
+def add(x,y):
+    results = x + y
+    return results
+addition = add(67,9)
+print(addition)'''
+
+
+def pow(base, exponent=2):
+    results = base**exponent
+    return results
+ans = pow(3,3)
+print(ans)
